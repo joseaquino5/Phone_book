22399b17e7532135203489a9ae0a86cb72379a89
Jose Aquino <jose.aquino@solvex.com.do>
2016-07-26 16:26:18 -0400

    Phone book!

---
+++ src/Contact.cpp
@@ -0,0 +1,22 @@
+/*
+ * Nodo.cpp
+ *
+ *  Created on: 23 jul. 2016
+ *      Author: José Aquino
+ */
+
+#include "Contact.h"
+
+namespace ITLA {
+
+	Contact::Contact() : _next(NULL)
+	{
+
+	}
+
+	Contact::~Contact()
+	{
+
+	}
+
+} /* namespace ITLA */
